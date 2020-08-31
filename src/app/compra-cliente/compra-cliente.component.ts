989ea0edad5d14b856535d657351409a6c18da72
Aurus Ortiz <horus91@live.com>
2020-08-30 23:56:06 -0600

    Logica en el controlador

--- src/app/compra-cliente/compra-cliente.component.ts
+++ src/app/compra-cliente/compra-cliente.component.ts
@@ -5,11 +5,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './compra-cliente.component.html',
   styleUrls: ['./compra-cliente.component.css']
 })
-export class CompraClienteComponent implements OnInit {
+export class CompraClienteComponent implements OnInit {  
+  Clientes = [];
+  Monto: number;
+  Nombre:string;
+  DUI:string;
+  Vehiculo:string;
+  desc:number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.desc= 0;
+    this.Clientes = [];
   }
 
+  registrarVisita(){    
+
+    this.Clientes.find(p=> p.DUI == this.DUI);
+    let cliente = this.Clientes.find(e => e.DUI === this.DUI);
+    if(cliente)
+    {          
+      cliente.Gastado += this.Monto;
+      cliente.Vehiculo = this.Vehiculo;
+      cliente.Visitas += 1;
+    }
+    else{
+      this.Clientes.push({DUI:this.DUI,Nombre:this.Nombre,Gastado:this.Monto,Vehiculo:this.Vehiculo, Visitas:1});
+    }
+    this.desc = this.Monto*0.05;
+  }
+
+}
+
+
+
+
+
+
+export class AppComponent {
 }
